43417cf412fec0f56e22328a46534df21a834852
Jennessa Ma <jennessa19ma@gmail.com>
2020-11-20 16:55:12 -0800

    carousel attempt

--- webapp_st.py
+++ webapp_st.py
@@ -1,8 +1,10 @@
 import streamlit as st
+import streamlit.components.v1 as components
 import cv2
 from PIL import Image
 import numpy as np
 import os
+import base64
 
 def detect_bugs(image):
     #Gray scale and blur the image
@@ -35,22 +37,59 @@ def detect_bugs(image):
     imgKeyPoints = cv2.drawKeypoints(img, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
     # Crop out keypoints
-    os.mkdir("images")
+    if not os.path.exists('immages'):
+        os.mkdir("immages")
+    filename = 0
     for keypoint in keypoints:
-        filename = 1
+        filename += 1
         x = int(keypoint.pt[0])
         y = int(keypoint.pt[1])
         size = int(keypoint.size)
         crop = img[max(1,y-2*size): min(height-1,y+2*size), max(1,x-2*size): min(width-1,x+2*size)]
         st.image(crop, use_column_width = False)
-        cv2.imwrite("images/" + str(filename) + ".jpg", crop)
-    # Display found keypoints
+        cv2.imwrite("immages/" + str(filename) + ".jpg", crop)
+
+    zipdir("immages")
     return imgKeyPoints
 
 #helper function to display local images
 def showImage(filepath):
     img = cv2.imread(filepath)
     st.image(img, width = 300)
+
+@st.cache(allow_output_mutation=True)
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+def zipdir(path):
+    # ziph is zipfile handle
+    # zipf = zipfile.ZipFile('tomato.zip','w')
+    #
+    # for root, dirs, files in os.walk(path):
+    #     for file in files:
+    #         zipf.write(os.path.join(root, file))
+    #
+    # zipf.close()
+
+    # href = f'<a href="data:file/zip;base64,{b64}" download=\'croppedbugs.zip\'>\
+    #     Click to download\
+    #     </a>'
+    href = '''
+    <style>
+    
+    </style>
+    <a href="#">Click here to download </a>
+    '''
+
+    # with open('tomato.zip', "rb") as f:
+    #     bytes = f.read()
+    #     b64 = base64.b64encode(bytes).decode()
+    #     href = f'<a href="data:file/zip;base64,{b64}" download=\'croppedbugs.zip\'>\
+    #         Click to download\
+    #         </a>'
+    st.markdown(href, unsafe_allow_html=True)
 
 def about():
     info = '''
@@ -85,11 +124,187 @@ def about():
         st.header("After")
         showImage("uploads/detected.jpg")
 
+    pic1 = get_base64_of_bin_file("uploads/detected.jpg")
+    pic2 = get_base64_of_bin_file("uploads/flypaper1.jpg")
+    pic3 = get_base64_of_bin_file("uploads/IMG_0041.jpg")
+    # components.html(
+    # """
+    # <head>
+    #   <meta name="viewport" content="width=device-width, initial-scale=1">
+    #   <style>
+    #     * {box-sizing: border-box}
+    #     body {font-family: Verdana, sans-serif; margin:0}
+    #     .mySlides {display: none}
+    #     img {vertical-align: middle;}
+    #
+    #     /* Slideshow container */
+    #     .slideshow-container {
+    #       max-width: 600px;
+    #       position: relative;
+    #       margin: auto;
+    #     }
+    #
+    #     /* Next & previous buttons */
+    #     .prev, .next {
+    #       cursor: pointer;
+    #       position: absolute;
+    #       top: 50%;
+    #       width: auto;
+    #       padding: 16px;
+    #       margin-top: -22px;
+    #       color: white;
+    #       font-weight: bold;
+    #       font-size: 18px;
+    #       transition: 0.6s ease;
+    #       border-radius: 0 3px 3px 0;
+    #       user-select: none;
+    #     }
+    #
+    #     /* Position the "next button" to the right */
+    #     .next {
+    #       right: 0;
+    #       border-radius: 3px 0 0 3px;
+    #     }
+    #
+    #     /* On hover, add a black background color with a little bit see-through */
+    #     .prev:hover, .next:hover {
+    #       background-color: rgba(0,0,0,0.8);
+    #     }
+    #
+    #     /* Caption text */
+    #     .text {
+    #       color: #f2f2f2;
+    #       font-size: 15px;
+    #       padding: 8px 12px;
+    #       position: absolute;
+    #       bottom: 8px;
+    #       width: 100%;
+    #       text-align: center;
+    #     }
+    #
+    #     /* Number text (1/3 etc) */
+    #     .numbertext {
+    #       color: #f2f2f2;
+    #       font-size: 12px;
+    #       padding: 8px 12px;
+    #       position: absolute;
+    #       top: 0;
+    #     }
+    #
+    #     /* The dots/bullets/indicators */
+    #     .dot {
+    #       cursor: pointer;
+    #       height: 15px;
+    #       width: 15px;
+    #       margin: 0 2px;
+    #       background-color: #bbb;
+    #       border-radius: 50%;
+    #       display: inline-block;
+    #       transition: background-color 0.6s ease;
+    #     }
+    #
+    #     .active, .dot:hover {
+    #       background-color: #717171;
+    #     }
+    #
+    #     /* Fading animation */
+    #     .fade {
+    #       -webkit-animation-name: fade;
+    #       -webkit-animation-duration: 1.5s;
+    #       animation-name: fade;
+    #       animation-duration: 1.5s;
+    #     }
+    #
+    #     @-webkit-keyframes fade {
+    #       from {opacity: .4}
+    #       to {opacity: 1}
+    #     }
+    #
+    #     @keyframes fade {
+    #       from {opacity: .4}
+    #       to {opacity: 1}
+    #     }
+    #
+    #     /* On smaller screens, decrease text size */
+    #     @media only screen and (max-width: 300px) {
+    #       .prev, .next,.text {font-size: 11px}
+    #     }
+    #   </style>
+    # </head>
+    # <body>
+    #
+    #   <div class="slideshow-container">
+    #
+    #     <div class="mySlides fade">
+    #       <div class="numbertext">1 / 3</div>
+    #       <img src="data:image/jpeg;base64, %s" style="width:100%">
+    #       <div class="text">Caption Text</div>
+    #     </div>
+    #
+    #     <div class="mySlides fade">
+    #       <div class="numbertext">2 / 3</div>
+    #       <img src="#" style="width:100%">
+    #       <div class="text">Caption Two</div>
+    #     </div>
+    #
+    #     <div class="mySlides fade">
+    #       <div class="numbertext">3 / 3</div>
+    #       <img src="#" style="width:100%">
+    #       <div class="text">Caption Three</div>
+    #     </div>
+    #
+    #     <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+    #     <a class="next" onclick="plusSlides(1)">&#10095;</a>
+    #
+    #   </div>
+    #   <br>
+    #
+    #   <div style="text-align:center">
+    #     <span class="dot" onclick="currentSlide(1)"></span>
+    #     <span class="dot" onclick="currentSlide(2)"></span>
+    #     <span class="dot" onclick="currentSlide(3)"></span>
+    #   </div>
+    #
+    #   <script>
+    #     var slideIndex = 1;
+    #     showSlides(slideIndex);
+    #
+    #     function plusSlides(n) {
+    #       showSlides(slideIndex += n);
+    #     }
+    #
+    #     function currentSlide(n) {
+    #       showSlides(slideIndex = n);
+    #     }
+    #
+    #     function showSlides(n) {
+    #       var i;
+    #       var slides = document.getElementsByClassName("mySlides");
+    #       var dots = document.getElementsByClassName("dot");
+    #       if (n > slides.length) {slideIndex = 1}
+    #       if (n < 1) {slideIndex = slides.length}
+    #       for (i = 0; i < slides.length; i++) {
+    #         slides[i].style.display = "none";
+    #       }
+    #       for (i = 0; i < dots.length; i++) {
+    #         dots[i].className = dots[i].className.replace(" active", "");
+    #       }
+    #       slides[slideIndex-1].style.display = "block";
+    #       dots[slideIndex-1].className += " active";
+    #     }
+    #   </script>
+    #
+    # </body>
+    #
+    # """ % pic1
+    # )
+    # components.html(slideshow)
+
 def main():
     background_img = '''
     <style>
         body {
-           background-image: url("https://lh3.googleusercontent.com/proxy/Ij_YGpsrh8AMukCLezadkJOFFx4Wup_weXfe23DkgHHiIsAnGHcoF9x_oZ42KDbWq-OyS5sEyYfopFUyq8XWm2JPgZ5B6sdeI4o3OxpN4STGS6JXEUvYyN2utKiAXrh10_ag");
+           background-image: url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSKqvjjmTFakcu3tyUj2UGchw40gH72VmGLjQ&usqp=CAU");
            background-size: cover;
            color: green;
         }
@@ -122,8 +337,6 @@ def main():
     	    image = Image.open(image_file)
     	    if st.button("Process"):
     		    detect_bugs(image=image)
-    			#st.image(result_img, use_column_width = True)
-    			#st.success("Found {} faces\n".format(len(result_faces)))
 
     elif choice == "About":
     	about()
